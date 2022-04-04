13f25eafcb38b150901ca16f9c637f655232a58b
Andre Maia <andrefnkmm@gmail.com>
2022-04-04 20:33:27 -0300

    Progresso do módulo de Android Jetpack do curso de componentes, layouts e UI/UX em Apps Android Nativos.

--- app/src/main/java/com/dio/myapplication/MainActivity.kt
+++ app/src/main/java/com/dio/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.dio.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dio.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
