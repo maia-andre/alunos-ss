1927fe005c5f98ff56c10ce6d7387d360ca1b28f
Andre Maia <andrefnkmm@gmail.com>
2022-04-06 21:43:18 -0300

    Final da aula de layout e algumas configs do App

---
+++ app/src/main/java/com/dio/myapplication/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.dio.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dio.myapplication.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
